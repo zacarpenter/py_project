2f462863e5b82de4099cba8e56b87ee9e0686406
Zac Carpenter <zacarpenter0810@gmail.com>
2019-05-13 15:57:31 -0400

    fixed typos

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -28,7 +28,7 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
-    #existing code omitted
+    # existing code omitted
     
     from . import db
     db.init_app(app)
