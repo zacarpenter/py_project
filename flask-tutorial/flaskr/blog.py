6609d332ad9e1f6e7612bca52fef20fd89a80075
Zac Carpenter <zacarpenter0810@gmail.com>
2019-10-09 16:53:59 -0400

    added missing comma

--- flask-tutorial/flaskr/blog.py
+++ flask-tutorial/flaskr/blog.py
@@ -12,7 +12,7 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username'
         'FROM post p JOIN user u ON p.author_id = u.id'
         'ORDER BY created DESC'
     ).fetchall()
