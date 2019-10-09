49b46ecc83dcedf55abcd3f6f435397404e6a83b
Zac Carpenter <zacarpenter0810@gmail.com>
2019-10-09 16:42:37 -0400

    Fixed: ModuleNotFoundError:'werkzeug.execeptions' Changed to exceptions

--- flask-tutorial/flaskr/blog.py
+++ flask-tutorial/flaskr/blog.py
@@ -1,7 +1,7 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
-from werkzeug.execeptions import abort
+from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
