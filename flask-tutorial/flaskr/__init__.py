ffc5657dd52f473343fb6a8950f34ece8af1c1be
Zac Carpenter <zacarpenter0810@gmail.com>
2019-10-09 15:32:50 -0400

    Finished the application. Needs testing to resolve any issues.

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -29,12 +29,15 @@ def create_app(test_config=None):
     def hello():
         return 'Hello, World!'
     # existing code omitted
-    
+
     from . import db
     db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
 
-    return app
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
+    return app
